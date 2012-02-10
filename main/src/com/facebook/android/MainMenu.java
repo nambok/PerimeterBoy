afc0be3f4622025fcd7d09c9f129646338377677
Nam Bok Rausis (Dell Laptop) <nambok@gmail.com>
2012-02-09 23:37:43 -0500

    Patched for Android 2.3.x

--- main/src/com/facebook/android/MainMenu.java
+++ main/src/com/facebook/android/MainMenu.java
@@ -1,17 +1,19 @@
 package com.facebook.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Criteria;
-import android.location.GpsStatus.Listener;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +21,8 @@ import android.widget.Toast;
 import android.app.ProgressDialog;
 
 public class MainMenu extends Activity implements View.OnClickListener {
-	
+		
+	private Handler mHandler;
 
 	//location manager
     public LocationManager lm;
@@ -37,11 +40,13 @@ public class MainMenu extends Activity implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        mHandler = new Handler();
 
         setContentView(R.layout.main_menu);
         
-        this.getLocation();
-        
+        getLocation();
+         
         viewPeopleBtn = (Button) findViewById(R.id.findPplBtn);
         viewPeopleBtn.setOnClickListener(this);
         
@@ -102,10 +107,21 @@ public class MainMenu extends Activity implements View.OnClickListener {
 		
 		super.finish();
 	}
+	
+	public void showToast(final String msg) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast toast = Toast.makeText(MainMenu.this, msg, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
     
-    public void getLocation() {
     	
-    	String ns = Context.NOTIFICATION_SERVICE;
+	public void getLocation() {
+        
+		String ns = Context.NOTIFICATION_SERVICE;
     	NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
     	int icon = R.drawable.icon;
     	CharSequence tickerText = "MyLocation";
@@ -125,9 +141,9 @@ public class MainMenu extends Activity implements View.OnClickListener {
 
     	mNotificationManager.notify(NOTIFICATION_ID, notification);
     	
-        /*
+		/*
          * launch a new Thread to get new location
-         */    	
+         */
         new Thread() {
             @Override
             public void run() {
@@ -145,55 +161,82 @@ public class MainMenu extends Activity implements View.OnClickListener {
                 criteria.setAccuracy(Criteria.ACCURACY_COARSE);
                 String provider = lm.getBestProvider(criteria, true);
                 if (provider != null && lm.isProviderEnabled(provider)) {
-                	lm.requestLocationUpdates(provider, 1, 0, locationListener,
+                    lm.requestLocationUpdates(provider, 300000, 0, locationListener,
                             Looper.getMainLooper());
-                    
                 } else {
+                    /*
+                     * GPS not enabled, prompt user to enable GPS in the
+                     * Location menu
+                     */
+                    new AlertDialog.Builder(MainMenu.this)
+                            .setTitle(R.string.enable_gps_title)
+                            .setMessage(getString(R.string.enable_gps))
+                            .setPositiveButton(R.string.gps_settings,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            startActivityForResult(
+                                                    new Intent(
+                                                            android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS),
+                                                    0);
+                                        }
+                                    })
+                            .setNegativeButton(R.string.cancel,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                            MainMenu.this.finish();
+                                        }
+                                    }).show();
                 }
+                Looper.loop();
             }
         }.start();
     }
-    
-    class MyLocationListener implements LocationListener {
-
-		@Override
-		public void onLocationChanged(Location loc) {
-			// TODO Auto-generated method stub
-			Utility.lat = new Double(loc.getLatitude());
-			Utility.lng = new Double(loc.getLongitude());
-			
-			if(Utility.userUID != null)
-			{
-				// TODO Auto-generated method stub
-				Bundle params = new Bundle();
-				Utility.mAsyncLocationRunner.request("location/update/id/" + Utility.userUID + "/lat/" + Utility.lat + "/lng/" + Utility.lng + "/format/json", params
-						, new BaseRequestLocationListener(){
 	
-							@Override
-							public void onComplete(String response, Object state) {
-								// TODO Auto-generated method stub
-								
-							}
-					
-				});
-			}
-		}
+	
+	class MyLocationListener implements LocationListener {
 
-		@Override
-		public void onProviderDisabled(String provider) {
-			// TODO Auto-generated method stub
-		}
+        @Override
+        public void onLocationChanged(Location loc) {
+            
+            // TODO Auto-generated method stub
+ 			Utility.lat = new Double(loc.getLatitude());
+ 			Utility.lng = new Double(loc.getLongitude());
+ 			
+ 			Bundle params = new Bundle();
+ 	        Utility.mAsyncLocationRunner.request("location/update/id/" + Utility.userUID + "/lat/" + Utility.lat + "/lng/" + Utility.lng + "/format/json", params, new LocationRequestListener());
+        }
 
-		@Override
-		public void onProviderEnabled(String provider) {
-			// TODO Auto-generated method stub
-			
-		}
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
 
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			// TODO Auto-generated method stub
-			
-		}
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
     }
+	
+	
+	 /*
+    *
+    */
+   public class LocationRequestListener extends BaseRequestLocationListener {
+
+       @Override
+       public void onComplete(final String response, final Object state) {
+           
+       }
+
+       public void onFacebookError(FacebookError error) {
+           
+           Toast.makeText(getApplicationContext(), "MyLocation Error: " + error.getMessage(),
+                   Toast.LENGTH_SHORT).show();
+       }
+   }
 }
